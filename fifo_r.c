f94a4825ceb315b2e4aeafdd42c4786cb5a99292
WYP9334 <1539221605@qq.com>
2019-03-14 11:22:09 +0800

    测试2

---
+++ fifo_r.c
@@ -0,0 +1,35 @@
+﻿//fifo_r.c
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+void sys_err(char *str, int exitno)
+{
+	perror(str);
+	exit(exitno);
+}
+
+int main(int argc, char *argv[])
+{
+	int fd, len;
+	char buf[1024];
+	if (argc < 2) {
+		printf("./fifo_r.out fifoname");
+		exit(1);
+	}
+	
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+		sys_err("open", 2);
+	
+	len = read(fd, buf, sizeof(buf));
+	write(STDOUT_FILENO, buf, len);
+	close(fd);
+	
+	return 0;
+}
---
+++ fifo_w.c
@@ -0,0 +1,34 @@
+//fifo_w.c
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+void sys_err(char *str, int exitno)
+{
+	perror(str);
+	exit(exitno);
+}
+
+int main(int argc, char *argv[])
+{
+	int fd;
+	char buf[1024] = "hello wyp\n";
+	if (argc < 2) {
+		printf("./fifo_w.out fifoname");
+		exit(1);
+	}
+	
+	fd = open(argv[1], O_WRONLY);
+	if (fd < 0)
+		sys_err("open", 1);
+	
+	write(fd, buf, strlen(buf));
+	close(fd);
+	
+	return 0;
+}
