707e6ce7558d73bf9e7d51c7141e1c7890f42f9a
WYP9334 <1539221605@qq.com>
2019-05-07 08:48:24 +0800

    添加mysleep功能

--- mysleep.c
+++ mysleep.c
@@ -1,24 +1,3 @@
-#include "wrap.h"
-#include <stdio.h>
-#include <sys/types.h>
-#include <signal.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <ctype.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <semaphore.h>
-#include <fcntl.h>
-/*套接字相关头文件*/
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netinet/in.h> //定义了INADDR_ANY
-
-#define SERVER_PORT     8000
-#define NUM     5
-
 unsigned int mysleep(unsigned int seconds)
 {
 	int ret;
