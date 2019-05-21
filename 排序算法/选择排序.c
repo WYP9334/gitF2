1dd6e28121ea2a9deba1359b3c830c3ebde34a48
WYP9334 <1539221605@qq.com>
2019-05-21 20:11:50 +0800

    完善

--- 排序算法/选择排序.c
+++ 排序算法/选择排序.c
@@ -1,3 +1,8 @@
+﻿/*选择排序（Selection sort）是一种简单直观的排序算法。
+ *它的工作原理是每一次从待排序的数据元素中选出最小（或最大）的一个元素，
+ *存放在序列的起始位置，直到全部待排序的数据元素排完。
+ */
+
 #include <stdio.h>
 
 void selectionSort(int *num, int len)  //选择排序升序排列
